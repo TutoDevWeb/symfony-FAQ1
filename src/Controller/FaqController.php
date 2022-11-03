13cbd4ce938e814d222eba0dc1c91dcad39d5f57
TutoDevWeb <TutoDevWeb@outlook.fr>
2022-11-03 15:00:36 +0100

    On va chercher le repository de QRs dans le controller et avec ça on récupère toutes les rabgées de la tables. Ensuite on les passe à twig et on les affiche dans twig avec une boucle for.

--- src/Controller/FaqController.php
+++ src/Controller/FaqController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\QR;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class FaqController extends AbstractController
 {
     #[Route('/', name: 'app_faq')]
-    public function faq(): Response
+    public function faq(ManagerRegistry $doctrine): Response
     {
+
+        $QRs = $doctrine->getRepository(QR::class)->findAll();
+
+        // dd($QRs);
+
         return $this->render('faq/faq.html.twig', [
             'controller_name' => 'FaqController',
+            'QRs' => $QRs
         ]);
     }
 }
